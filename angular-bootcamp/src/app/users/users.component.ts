02f508f5c65697f87dac01f42bfb371a9fc6fe49
gpaparaz <giorgia.paparazzo@gmail.com>
2023-08-25 13:44:26 +0200

    pipe
    dichiarato array di users e utilizzato un pipe ( | )  per mostrare il json di utenti. senza il pipe si sarebbe visualizzato [Object object]. il pipe serve per passare una funziona a quell'oggetto che si vuole mostrare a video

--- angular-bootcamp/src/app/users/users.component.ts
+++ angular-bootcamp/src/app/users/users.component.ts
@@ -2,9 +2,52 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-users',
-  templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+
+  templateUrl: 'users.component.html',
+  styleUrls: [
+    'users.component.css'
+  ]
 })
+
 export class UsersComponent {
+  title = 'Users'
+  public users = [
+    {
+      name: 'Hidran1',
+      lastname: 'Arias',
+      email: 'hidran@gmail.com',
+      fiscalcode: 'RSAHRN72M22Z444S',
+      province: 'Torino',
+      phone: '454545455',
+      age: 43
+    },
+    {
+      name: 'Hidran2',
+      lastname: 'Arias',
+      email: 'hidran@gmail.com',
+      fiscalcode: 'RSAHRN72M22Z444S',
+      province: 'Torino',
+      phone: '454545455',
+      age: 43
+    },
+    {
+      name: 'Hidran3',
+      lastname: 'Arias',
+      email: 'hidran@gmail.com',
+      fiscalcode: 'RSAHRN72M22Z444S',
+      province: 'Torino',
+      phone: '454545455',
+      age: 43
+    },
+    {
+      name: 'Hidran4',
+      lastname: 'Arias',
+      email: 'hidran@gmail.com',
+      fiscalcode: 'RSAHRN72M22Z444S',
+      province: 'Torino',
+      phone: '454545455',
+      age: 43
+    }
+  ];
 
 }
