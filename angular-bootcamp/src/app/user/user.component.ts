cc915e93bfa56ce35f72750297d44d882663010b
gpaparaz <giorgia.paparazzo@gmail.com>
2023-08-26 11:58:44 +0200

    emit event from child to parent
    quando il parent invia al figlio si usano le parentesi quadre;
    quando un parent ascolta/riceve dal figlio allora si usano le parentesi tonde. 
    
    come fa il figlio ad emettere un evento? si usa @Output
    
    il padre ascolta un evento che si chiama (onDeleteUser); quando lo ascolta chiama una funzione propria che si chiama onDeleteUser (è stata chiamata allo stesso modo), e tramite l'argmento $event riceve il dato che il figlio gli sta passando

--- angular-bootcamp/src/app/user/user.component.ts
+++ angular-bootcamp/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { UsersService } from '../users.service';
 
 @Component({
@@ -7,15 +7,21 @@ import { UsersService } from '../users.service';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-
+  //potrei anche fare a meno di specificare qualcosa nelle parentesi tonde e al parent indicare solo 'user' invece di 'user-data'
   @Input('user-data') user: any;
+
+  @Output('onDeleteUser') userDeleted = new EventEmitter(); //NB: la libreria è di angular/core
+  //perchè questo evento fuoriesca da questo componente deve essere di tipo Output, e quindi può essere ascltato dal padre
+  //userDeleted è la variabile qui interna, mentre onDeleteUser serve al padre per ascoltare
+
+
   constructor(private userService: UsersService) { }
 
   ngOnInit(): void {
   }
 
   deleteUser(user:any) {
-    this.userService.deleteUser(this.user);
+    this.userDeleted.emit(this.user);
   }
 
 }
--- angular-bootcamp/src/app/users/users.component.ts
+++ angular-bootcamp/src/app/users/users.component.ts
@@ -24,4 +24,8 @@ export class UsersComponent implements OnInit{
 
   }
 
+  onDeleteUser(user:any){
+    this.service.deleteUser(user);
+  }
+
 }
